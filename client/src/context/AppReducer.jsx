3ca85c2e95edfca8b734002d8eba91950f871e38
LucassNogueira <simplylucasjason@gmail.com>
2022-03-10 16:31:51 -0600

    fixed spelling, possibly fixed delete

--- client/src/context/AppReducer.jsx
+++ client/src/context/AppReducer.jsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line import/no-anonymous-default-export
+/* eslint-disable import/no-anonymous-default-export */
 export default (state, action) => {
   switch (action.type) {
     case "GET_TRANSACTIONS":
